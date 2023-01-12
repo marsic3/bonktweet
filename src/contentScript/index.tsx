48b5fdf7fe8acbaf082693e31d0239c4ad754325
infin1t3 <jovanovicv90@gmail.com>
2023-01-12 21:29:26 +0100

    Working version.

--- src/contentScript/index.tsx
+++ src/contentScript/index.tsx
@@ -3,6 +3,13 @@ import { createRoot } from "react-dom/client";
 import "../assets/tailwind.css";
 import ContentScript from "./contentScript";
 
+declare global {
+  interface Window {
+    phantom: any
+    foo: any
+  }
+}
+
 function init() {
   const appContainer = document.createElement("div");
   if (!appContainer) {
@@ -11,7 +18,12 @@ function init() {
 
   const articleList = document.querySelectorAll("article");
   // const root = createRoot(appContainer);
-
+    var s = document.createElement('script');
+    s.src = chrome.runtime.getURL('./script.js');
+    s.onload = function() {
+      s.remove();
+    };
+    document.body.appendChild(s);
   for (const element of articleList) {
     var btn = document.createElement("button");
     btn.appendChild(document.createTextNode("Bonk tweet"));
@@ -20,6 +32,7 @@ function init() {
       chrome.runtime.sendMessage({ greeting: "hello" }, function (response) {
         console.log(response.farewell);
         console.log(window);
+
       });
     });
 
---
+++ src/static/script.js
@@ -0,0 +1,3 @@
+(function () {
+  console.log(window.phantom.solana.connect())
+})();
