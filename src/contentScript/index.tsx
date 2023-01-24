9e5986abd270015f221d1c953531bbb1271c876f
marsic3 <milos.arsic@lotusflare.com>
2023-01-24 18:04:10 +0100

    only bonks

--- src/contentScript/index.tsx
+++ src/contentScript/index.tsx
@@ -27,86 +27,109 @@ function init() {
   });
 
   for (const tweet of articleList) {
-    var btn = document.createElement("button");
-    var img = document.createElement("img");
-    img.src = chrome.runtime.getURL("./bonkbtn.png");
-    img.style.maxWidth = "100px";
-    img.style.height = "auto";
-    btn.appendChild(img);
-    tweet.setAttribute("name", "Bonk");
-    const tweetElement = tweet.querySelector(
-      "div.css-1dbjc4n.r-18u37iz.r-1wbh5a2.r-13hce6t > div > div.css-1dbjc4n.r-18u37iz.r-1q142lx > a"
+    const spanItem = tweet.querySelector(
+      "div > div > div > div.css-1dbjc4n.r-18u37iz > div.css-1dbjc4n.r-1iusvr4.r-16y2uox.r-1777fci.r-kzbkwu > div:nth-child(2) > div.css-1dbjc4n > div.css-901oao"
     );
-    let tweetId;
-    if (
-      tweetElement &&
-      tweetElement.outerHTML &&
-      tweetElement.outerHTML.match(/\/status\/([0-9]+)/)
-    ) {
-      tweetId = tweetElement.outerHTML.match(/\/status\/([0-9]+)/)[1];
+    const listOfSpans = spanItem.querySelectorAll("span");
+    for (const spanItem of listOfSpans) {
+      console.log(spanItem.textContent, "spanItem");
+
+      if (spanItem.textContent.toLowerCase().includes("bonk")) {
+        var btn = document.createElement("button");
+        var img = document.createElement("img");
+        img.src = chrome.runtime.getURL("./bonkbtn.png");
+        img.style.maxWidth = "100px";
+        img.style.height = "auto";
+        btn.appendChild(img);
+        tweet.setAttribute("name", "Bonk");
+        const tweetElement = tweet.querySelector(
+          "div.css-1dbjc4n.r-18u37iz.r-1wbh5a2.r-13hce6t > div > div.css-1dbjc4n.r-18u37iz.r-1q142lx > a"
+        );
+        let tweetId;
+        if (
+          tweetElement &&
+          tweetElement.outerHTML &&
+          tweetElement.outerHTML.match(/\/status\/([0-9]+)/)
+        ) {
+          tweetId = tweetElement.outerHTML.match(/\/status\/([0-9]+)/)[1];
+        }
+
+        btn.addEventListener("click", async function () {
+          injectedScript.src = chrome.runtime.getURL("./script.js");
+
+          injectedScript.onload = function () {
+            injectedScript.remove();
+          };
+          document.body.appendChild(injectedScript);
+
+          const bonktweet = (await buyTweet(
+            new PublicKey(wallet),
+            tweetId
+          )) as any;
+          document.dispatchEvent(
+            new CustomEvent("getTransaction", { detail: bonktweet })
+          );
+        });
+        tweet.appendChild(btn);
+      }
     }
-
-    btn.addEventListener("click", async function () {
-      injectedScript.src = chrome.runtime.getURL("./script.js");
-
-      injectedScript.onload = function () {
-        injectedScript.remove();
-      };
-      document.body.appendChild(injectedScript);
-
-      const bonktweet = (await buyTweet(new PublicKey(wallet), tweetId)) as any;
-      document.dispatchEvent(
-        new CustomEvent("getTransaction", { detail: bonktweet })
-      );
-    });
-    tweet.appendChild(btn);
   }
 
   var observer = new MutationObserver(function (mutations) {
-    mutations.forEach(function (mutation) {
+    mutations.forEach(function (mutation: any) {
       if (mutation.type === "childList") {
-        // @ts-ignore
         if (mutation.target.style.position === "relative") {
           const articleList = document.querySelectorAll("article");
           for (const tweet of articleList) {
-            let button = tweet.querySelector("button");
-            if (!button) {
-              var btn = document.createElement("button");
-              var img = document.createElement("img");
-              img.src = chrome.runtime.getURL("./bonkbtn.png");
-              img.style.maxWidth = "100px";
-              img.style.height = "auto";
-              btn.appendChild(img);
-              tweet.setAttribute("name", "Bonk");
-              const tweetElement = tweet.querySelector(
-                "div.css-1dbjc4n.r-18u37iz.r-1wbh5a2.r-13hce6t > div > div.css-1dbjc4n.r-18u37iz.r-1q142lx > a"
-              );
-              let tweetId;
-              if (
-                tweetElement &&
-                tweetElement.outerHTML &&
-                tweetElement.outerHTML.match(/\/status\/([0-9]+)/)
-              ) {
-                tweetId = tweetElement.outerHTML.match(/\/status\/([0-9]+)/)[1];
+            const spanItem = tweet.querySelector(
+              "div > div > div > div.css-1dbjc4n.r-18u37iz > div.css-1dbjc4n.r-1iusvr4.r-16y2uox.r-1777fci.r-kzbkwu > div:nth-child(2) > div.css-1dbjc4n > div.css-901oao"
+            );
+            const listOfSpans = spanItem.querySelectorAll("span");
+            for (const spanItem of listOfSpans) {
+              console.log(spanItem.textContent, "spanItem");
+
+              if (spanItem.textContent.toLowerCase().includes("bonk")) {
+                let button = tweet.querySelector("button");
+                if (!button) {
+                  var btn = document.createElement("button");
+                  var img = document.createElement("img");
+                  img.src = chrome.runtime.getURL("./bonkbtn.png");
+                  img.style.maxWidth = "100px";
+                  img.style.height = "auto";
+                  btn.appendChild(img);
+                  tweet.setAttribute("name", "Bonk");
+                  const tweetElement = tweet.querySelector(
+                    "div.css-1dbjc4n.r-18u37iz.r-1wbh5a2.r-13hce6t > div > div.css-1dbjc4n.r-18u37iz.r-1q142lx > a"
+                  );
+                  let tweetId;
+                  if (
+                    tweetElement &&
+                    tweetElement.outerHTML &&
+                    tweetElement.outerHTML.match(/\/status\/([0-9]+)/)
+                  ) {
+                    tweetId =
+                      tweetElement.outerHTML.match(/\/status\/([0-9]+)/)[1];
+                  }
+
+                  btn.addEventListener("click", async function () {
+                    injectedScript.src = chrome.runtime.getURL("./script.js");
+
+                    injectedScript.onload = function () {
+                      injectedScript.remove();
+                    };
+                    document.body.appendChild(injectedScript);
+
+                    const bonktweet = (await buyTweet(
+                      new PublicKey(wallet),
+                      tweetId
+                    )) as any;
+                    document.dispatchEvent(
+                      new CustomEvent("getTransaction", { detail: bonktweet })
+                    );
+                  });
+                  tweet.appendChild(btn);
+                }
               }
-
-              btn.addEventListener("click", async function () {
-                injectedScript.src = chrome.runtime.getURL("./script.js");
-
-                injectedScript.onload = function () {
-                  injectedScript.remove();
-                };
-                document.body.appendChild(injectedScript);
-
-                const bonktweet = (await buyTweet(
-                  new PublicKey(wallet),
-                  tweetId
-                )) as any;
-                document.dispatchEvent(
-                  new CustomEvent("getTransaction", { detail: bonktweet })
-                );
-              });
-              tweet.appendChild(btn);
             }
           }
         }
